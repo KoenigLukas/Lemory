09cce0a81047fa8ffaa4d06a7a51c500da2aa0eb
Lukas König <lukas.koenig02@gmail.com>
2019-05-11 01:00:03 +0200

    added Routers

--- API/app.js
+++ API/app.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/user/loginUser');
+const registerRouter = require('./routes/user/registerUser');
+const getStatsRouter = require('./routes/user/stats/getStats');
+const setStatsRouter = require('./routes/user/stats/setStats');
+const usersRouter = require('./routes/users');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users',usersRouter);
+app.use('/user/login', loginRouter);
+app.use('/user/register', registerRouter);
+app.use('/user/stats/get', getStatsRouter);
+app.use('/user/stats/set', setStatsRouter);
+
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!');
+});
 
 module.exports = app;
