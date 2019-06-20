bbc59ccd14e491ca6f953d0c2986d8d76d6b1cc2
Lukas König <43269485+KoenigLukas@users.noreply.github.com>
2019-06-20 20:50:32 +0200

    Update Lemory.sql

--- Lemory_backend_SQL_schema/Lemory.sql
+++ Lemory_backend_SQL_schema/Lemory.sql
@@ -7,40 +7,25 @@ USE `lemory`;
 -- ------------------------------------------------------
 -- Server version	8.0.15
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 
 --
 -- Table structure for table `stats`
 --
 
 DROP TABLE IF EXISTS `stats`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `stats` (
   `usernr` int(11) NOT NULL,
   `won` boolean DEFAULT FALSE,
   `time` int(11) DEFAULT NULL,
--- New Created and LastUpdate
   `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `stats`
 --
 
 LOCK TABLES `stats` WRITE;
-/*!40000 ALTER TABLE `stats` DISABLE KEYS */;
-/*!40000 ALTER TABLE `stats` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -48,8 +33,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `user` (
   `username` varchar(48) NOT NULL,
   `passwd` varchar(65) NOT NULL,
@@ -65,24 +48,13 @@ CREATE TABLE `user` (
   UNIQUE KEY `usernr_UNIQUE` (`usernr`),
   UNIQUE KEY `email_UNIQUE` (`email`)
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user`
 --
 
 LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2019-05-10 16:51:15
