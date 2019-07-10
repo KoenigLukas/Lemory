47e0cbefd66c90e42162a5208efb203e44bab2a9
Lukas König <lukas.koenig02@gmail.com>
2019-07-10 20:37:47 +0200

    added new logging to API

--- Lemory_API/server.ts
+++ Lemory_API/server.ts
@@ -1,15 +1,19 @@
 import express = require('express');
 import logger = require("morgan");
+import * as winston from "winston";
+import expressWinston, { LoggerOptions } from 'express-winston';
 import * as dotenv from "dotenv";
+
 dotenv.config();
 
-import v1registerRouter = require('./v1/routes/user/registerUser');
-import v1loginRouter = require('./v1/routes/user/loginUser');
-import v1getStatsRouter = require('./v1/routes/user/stats/getStats');
-import v1putStatsRouter = require('./v1/routes/user/stats/putStats');
-import v1getBestTimeRouter = require('./v1/routes/user/stats/getBestTime');
-import v1getNumPlayedGamesRouter = require('./v1/routes/user/stats/getTimesPlayed');
-import v1resetStatsRouter = require('./v1/routes/user/stats/resetStats');
+import registerRouter = require('./v1/routes/user/registerUser');
+import loginRouter = require('./v1/routes/user/loginUser');
+import getStatsRouter = require('./v1/routes/user/stats/getStats');
+import putStatsRouter = require('./v1/routes/user/stats/putStats');
+import getBestTimeRouter = require('./v1/routes/user/stats/getBestTime');
+import getNumPlayedGamesRouter = require('./v1/routes/user/stats/getTimesPlayed');
+import resetStatsRouter = require('./v1/routes/user/stats/resetStats');
+
 
 
 // @ts-ignore
@@ -17,21 +21,35 @@ const server = express();
 
 const port = process.env.PORT || 3000;
 
-server.use(logger('dev'));
+server.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'combined.log' })
+    ],
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.json()
+    ),
+    meta: true,
+    msg: "HTTP {{req.method}} {{req.url}}",
+    expressFormat: true,
+    colorize: false,
+    ignoreRoute: function (req, res) { return false; }
+}));
+
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-server.use('/api/v1/user/login', v1loginRouter);
-server.use('/api/v1/user/register', v1registerRouter);
-server.use('/api/v1/user/stats/get', v1getStatsRouter);
-server.use('/api/v1/user/stats/put', v1putStatsRouter);
-server.use('/api/v1/user/stats/getbesttime', v1getBestTimeRouter);
-server.use('/api/v1/user/stats/gettimesplayed', v1getNumPlayedGamesRouter);
-server.use('/api/v1/user/stats/reset', v1resetStatsRouter);
-
+server.use('/api/v1/user/login', loginRouter);
+server.use('/api/v1/user/register', registerRouter);
+server.use('/api/v1/user/stats/get', getStatsRouter);
+server.use('/api/v1/user/stats/put', putStatsRouter);
+server.use('/api/v1/user/stats/getbesttime', getBestTimeRouter);
+server.use('/api/v1/user/stats/gettimesplayed', getNumPlayedGamesRouter);
+server.use('/api/v1/user/stats/reset', resetStatsRouter);
 
 
 server.listen(port, function () {
-    console.log('Example server listening on port ' + port + '!');
+    console.log('Server is listening on Port ' + port);
 });
 
